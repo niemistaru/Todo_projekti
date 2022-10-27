49da277c51e63a5a34aca2d72b5b9fbcf7e24f90
niemistaru <80707438+niemistaru@users.noreply.github.com>
2022-10-27 20:04:56 +0300

    Add files via upload

---
+++ todojs.js
@@ -0,0 +1,122 @@
+//array for todo-items
+let todoItems = [];
+
+//render todos on screen
+function renderTodo(todo) {
+    //local storage
+    localStorage.setItem('todoItemsRef', JSON.stringify(todoItems));
+
+    //select first element on to do-list  TARU KOKEILIN NYT UL-ELEMENTIN LUOKKAA
+    const list = document.querySelector('.todo-items');
+    const item = document.querySelector(`[data-key='${todo.id}']`);
+
+    if (todo.deleted) {
+        item.remove();
+        return
+    }
+
+    const isCompleted = todo.completed ? 'done':'';
+    const node = document.createElement("li");
+    node.setAttribute('class', `todo-item ${isCompleted}`);
+    node.setAttribute('data-key', todo.id);
+    node.innerHTML = `
+        <input id="${todo.id}" type="checkbox"/>
+        <label for="${todo.id}" class="tick js-tick"></label>
+        <span>${todo.text}</span>
+        <button class="delete-button">X</button>
+    `;
+    //OTIN IMPUTIN JA SPANIN VÄLISTÄ TÄN, MITÄ TÄÄ TEKEE?
+    //
+    //taru tossa tuo tick-juttu ei sit oo siinä vanhassa koodissa, ni tsekkaa pitääks olla joku muu
+    // delete-nappaulan alla yllä olis tämmöne <svg><use href="#delete-icon"></use></svg>
+    if (item) {
+        list.replaceChild(node, item);
+    } else {
+        list.append(node);
+    }
+
+}
+
+
+// add todo
+function addTodo(text) {
+    const todo = {
+        text,
+        id: Date.now(),
+        completed: false,
+};
+todoItems.push(todo);
+renderTodo(todo);
+}
+
+//toggle checked
+function toggleChecked(key) {
+    const index = todoItems.findIndex(item => item.id === Number(key));
+    todoItems[index].completed = !todoItems[index].completed;
+    renderTodo(todoItems[index]);
+}
+
+function deleteTodo(key) {
+    const index = todoItems.findIndex(item => item.id === Number(key));
+    const todo = {
+        deleted: true,
+        ...todoItems[index]
+    };
+    todoItems = todoItems.filter(item => item.id !== Number(key));
+    renderTodo(todo);
+}
+
+//select form
+const form =document.querySelector('.todo-form');
+//submit event listener
+form.addEventListener('submit', event => {
+    //prevent refreshing
+    event.preventDefault();
+    //select text input
+    const input = document.querySelector('.todo-input');
+
+    //get the input value, remove whitespace
+    const text = input.value.trim();
+    if (text !== '') {
+        addTodo(text);
+        input.value = '';
+        input.focus();
+    }
+
+});
+
+//mark as completed TARU on kokeillut vaihtaa todo-itemsin tilalle todo-input, se ei toimi
+const list = document.querySelector('.todo-items');
+list.addEventListener('click', event => {
+    if (event.target.classList.contains('js-tick')){ 
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleChecked(itemKey);
+}
+
+/*
+const list = document.querySelector('.todo-items');
+list.addEventListener('click', event => {
+    if (event.target.classList.contains('js-tick')){
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleChecked(itemKey);
+} 
+ ONKS TÄÄ NYT SAATANA KOKO BLOKKI TURHA HÄ*/
+
+if (event.target.classList.contains('delete-button')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+}
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const ref =  localStorage.getItem('todoItemsRef');
+    if (ref){
+       todoItems = JSON.parse(ref);
+       todoItems.forEach(t => {
+        renderTodo(t);
+       }); 
+    }
+});
+
+//ADD TO LOCAL STORAGE, NÄÄ ON NYT IHAN SUN OMIA!!!!
+
